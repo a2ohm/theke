aa9f11404385b8c015ba1a60a353396e4290c2a4
Antoine <antoine@2ohm.fr>
2021-04-04 18:20:40 +0200

    Fix: parsing of raw uri

--- theke/uri.py
+++ theke/uri.py
@@ -3,10 +3,15 @@ from urllib.parse import unquote, quote
 
 class ThekeURI:
     def __init__(self, URI, isRaw = False):
-        self.URIprefix, self.path, self.params = self.parse_URI(URI)
+        if isRaw:
+            self.prefix, self.path, self.params = self.parse_URI(unquote(URI))
+        else:
+            self.prefix, self.path, self.params = self.parse_URI(URI)
 
-        if self.URIprefix != 'theke:///':
-            raise ValueError("Unsupported ThekeURI ({})".format(self.URIprefix))
+        validPrefix = ['theke:///', 'sword:///']
+
+        if self.prefix not in validPrefix:
+            raise ValueError("Unsupported ThekeURI ({})".format(self.prefix))
 
     def parse_URI(self, URI):
         """Parse the URI.
@@ -31,25 +36,29 @@ class ThekeURI:
 
     def get_decoded_URI(self):
         if len(self.params) > 0:
-            return self.URIprefix + self.path + '?' + '&'.join([name + '=' + value for name, value in self.params.items()])
+            return self.prefix + self.path + '?' + '&'.join([name + '=' + value for name, value in self.params.items()])
         else:
-            return self.URIprefix + self.path
+            return self.prefix + self.path
 
     def get_coded_URI(self):
         if len(self.params) > 0:
-            return self.URIprefix + quote(self.path) + '?' + '&'.join([name + '=' + quote(value) for name, value in self.params.items()])
+            return self.prefix + quote(self.path) + '?' + '&'.join([name + '=' + quote(value) for name, value in self.params.items()])
         else:
-            return self.URIprefix + quote(self.path)
+            return self.prefix + quote(self.path)
 
     def __repr__(self):
         return self.get_decoded_URI()
 
 
 if __name__ == "__main__":
-    uri1 = ThekeURI("theke:///bible/John 1:1?source=MorphGNT")
+    uri1 = ThekeURI("sword:///bible/John 1:1?source=MorphGNT")
     print(uri1.get_decoded_URI())
     print(uri1.get_coded_URI())
 
     uri2 = ThekeURI("theke:///welcome.html")
     print(uri2.get_decoded_URI())
     print(uri2.get_coded_URI())
+
+    uri3 = ThekeURI("sword:///bible/John%201%3A1?source=MorphGNT", isRaw=True)
+    print(uri3.get_decoded_URI())
+    print(uri3.get_coded_URI())
