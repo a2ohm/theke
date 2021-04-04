57adef734e74a70bf43a23cddbc6e7a634804e54
Antoine <antoine@2ohm.fr>
2021-04-04 19:27:12 +0200

    Add a very basic gotobar

--- theke/gui/mainwindow.py
+++ theke/gui/mainwindow.py
@@ -24,22 +24,28 @@ class ThekeWindow(Gtk.ApplicationWindow):
 
         self._theke_window_main = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 0)
 
+        self.navigationbar = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 0)
         self.scrolled_window = Gtk.ScrolledWindow()
         self.statusbar = Gtk.Statusbar()
 
-        self.webview = WebKit2.WebView()
-        self.contentManager = self.webview.get_user_content_manager()
+        self.gotobar = Gtk.SearchEntry()
+        self.gotobar.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "go-next")
+        self.gotobar.connect("activate", self.handleGoto)
 
+        self.webview = WebKit2.WebView()
         self.webview.connect("decide-policy", self.decidePolicy)
 
         # Add css
+        self.contentManager = self.webview.get_user_content_manager()
         self.styleSheet = WebKit2.UserStyleSheet(css,
             WebKit2.UserContentInjectedFrames.ALL_FRAMES,
             WebKit2.UserStyleLevel.USER,
             None, None)
         self.contentManager.add_style_sheet(self.styleSheet)
 
+        self.navigationbar.add(self.gotobar)
         self.scrolled_window.add(self.webview)
+        self._theke_window_main.pack_start(self.navigationbar, False, True, 0)
         self._theke_window_main.pack_start(self.scrolled_window, True, True, 0)
         self._theke_window_main.pack_start(self.statusbar, False, True, 0)
         self.add(self._theke_window_main)
@@ -70,3 +76,12 @@ class ThekeWindow(Gtk.ApplicationWindow):
                 return True
 
         return False
+
+    def handleGoto(self, entry):
+        '''@param entry: the object which received the signal.
+        '''
+
+        #TOFIX. Suppose that the content of the gotobar is a valid Sword Key
+        key = entry.get_text()
+        uri = theke.uri.ThekeURI("sword:///bible/{}".format(key))
+        self.load_uri(uri)
