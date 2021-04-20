aed1c74359623a69ad0764ae95c30000191f4afc
Antoine <antoine@2ohm.fr>
2021-04-20 21:45:57 +0200

    HistoryBar: rotate buttons if uri already in the history bar

--- theke/gui/widget_ThekeHistoryBar.py
+++ theke/gui/widget_ThekeHistoryBar.py
@@ -20,23 +20,6 @@ class ThekeHistoryBar(Gtk.ButtonBox):
         try:
             historyIndex = self.history.index((label, uri))
 
-        except ValueError:
-            # This uri does not exist in the history
-            if len(self.history) >= MAX_NUMBER_OF_BUTTONS:
-                self.history.popleft()
-                self.remove(self.get_children()[0])
-
-            self.history.append((label, uri))
-
-            button = Gtk.Button(label=label, use_underline=False)
-            button.set_tooltip_text(str(uri))
-            button.get_child().set_ellipsize(Pango.EllipsizeMode.MIDDLE)
-            button.uri = uri
-            button.connect('clicked', self.on_button_clicked)
-            button.show_all()
-            self.add(button)
-
-        else:
             # The visited uri is already in the history,
             # move its button at the end of the bar
             if historyIndex < MAX_NUMBER_OF_BUTTONS-1:
@@ -44,6 +27,24 @@ class ThekeHistoryBar(Gtk.ButtonBox):
                 del self.history[historyIndex]
                 self.history.append((label, uri))
                 self.reorder_child(self.get_children()[historyIndex], -1)
+
+        except ValueError:
+            # This uri does not exist in the history
+            if len(self.history) >= MAX_NUMBER_OF_BUTTONS:
+                self.history.popleft()
+                self.remove(self.get_children()[0])
+
+            self.history.append((label, uri))
+            button = Gtk.Button(label=label, use_underline=False)
+            button.set_tooltip_text(str(uri))
+            button.uri = uri
+
+            button.connect('clicked', self.on_button_clicked)
+            button.show_all()
+
+            self.pack_start(button, True, True, 0)
+
+            
 
     def on_button_clicked(self, button):
         self.thekeWindow.load_uri(button.uri)
