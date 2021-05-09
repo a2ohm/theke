66638a6f9e8ca9ffceb6c0b9a36a51560a6cc9c1
Antoine <antoine@2ohm.fr>
2021-05-09 20:07:32 +0200

    Parse verbs tence

--- theke/morphology/oshm_morph.py
+++ theke/morphology/oshm_morph.py
@@ -1,34 +1,50 @@
 def parse_oshm(morph):
-    analysis = ''
+    analysis = []
 
     for subMorph in morph[1:].split('_'):
-        wordClass = subMorph[0]
-
-        if wordClass == 'A':
-            analysis += "adjectif"
-
-        elif wordClass == 'C':
-            analysis += "conjonction ; "
-
-        elif wordClass == 'D':
-            analysis += "adverbe"
-
-        elif wordClass == 'N':
-            analysis += "nom"
-
-        elif wordClass == 'P':
-            analysis += "pronom démonstratif"
-
-        elif wordClass == 'R':
-            analysis += "préposition"
-
-        elif wordClass == 'T':
-            analysis += "particule"
-
-        elif wordClass == 'V':
-            analysis += "verbe"
-            
-        else:
-            analysis += "inconnu"
+        analysis.append(wordClasses.get(subMorph[0], lambda x: "inconnu")(subMorph))
     
     return analysis
+
+def parse_oshm_verb(subMorph):
+    return "verbe ({})".format(tences.get(subMorph[1], '?'))
+
+wordClasses = {
+    'A': lambda x: "adjectif",
+    'C': lambda x: "conjonction",
+    'D': lambda x: "adverbe",
+    'N': lambda x: "nom",
+    'P': lambda x: "pronom démonstratif",
+    'R': lambda x: "préposition",
+    'S': lambda x: "suffixe",
+    'T': lambda x: "particule",
+    'V': parse_oshm_verb,
+}
+
+tences = {
+    'D': "nithpael (?)",
+    'H': "hof'al",
+    'K': "pulal (?)",
+    'L': "polpal (?)",
+    'M': "poal (?)",
+    'N': "nif'al",
+    'O': "polal (?)",
+    'P': "pu'al",
+    'Q': "qal",
+    'c': "tiphil (?)",
+    'f': "hithpalpel",
+    'h': "hif'il",
+    'i': "pilel (?)",
+    'j': "pealal (?)",
+    'k': "palel (?)",
+    'l': "pilpel (?)",
+    'm': "poel (?)",
+    'o': "polel (?)",
+    'p': "pi'el",
+    'q': "qal",
+    'r': "hithpolel (?)",
+    't': "hithpael (?)",
+    'u': "hothpaal (?)",
+    'v': "hishthaphel (?)",
+    'z': "hitpoel (?)",
+}
