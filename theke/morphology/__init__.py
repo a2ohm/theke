d58da0c048c07de35e80bb83c10d823df71188ec
Antoine <antoine@2ohm.fr>
2021-05-09 20:12:07 +0200

    Print robinson morphology

--- theke/morphology/__init__.py
+++ theke/morphology/__init__.py
@@ -1,4 +1,5 @@
 from theke.morphology.oshm_morph import parse_oshm
+from theke.morphology.robinson_morph import parse_robinson
 
 def parse(morph):
     '''Parse a morphology string.
@@ -8,7 +9,10 @@ def parse(morph):
     '''
     morph = morph.split(':')
 
-    if len(morph) > 1 and morph[0] == 'oshm':
-        return parse_oshm(morph[1])
+    if len(morph) > 1:
+        if morph[0] == 'oshm':
+            return parse_oshm(morph[1])
+        elif morph[0] == 'robinson':
+            return parse_robinson(morph[1])
     else:
         return None
