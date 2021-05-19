0547fc59e681116ceac0a55bf617a37075b7da19
Antoine <antoine@2ohm.fr>
2021-05-19 12:07:46 +0200

    Nouns morphological analysis

--- theke/morphology/oshm_morph.py
+++ theke/morphology/oshm_morph.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_oshm(morph):
     analysis = []
 
@@ -5,21 +7,6 @@ def parse_oshm(morph):
         analysis.append(wordClasses.get(subMorph[0], lambda x: "inconnu")(subMorph))
     
     return analysis
-
-def parse_oshm_verb(subMorph):
-    return "verbe ({})".format(tences.get(subMorph[1], '?'))
-
-wordClasses = {
-    'A': lambda x: "adjectif",
-    'C': lambda x: "conjonction",
-    'D': lambda x: "adverbe",
-    'N': lambda x: "nom",
-    'P': lambda x: "pronom démonstratif",
-    'R': lambda x: "préposition",
-    'S': lambda x: "suffixe",
-    'T': lambda x: "particule",
-    'V': parse_oshm_verb,
-}
 
 tences = {
     'D': "nithpael (?)",
@@ -47,4 +34,57 @@ tences = {
     'u': "hothpaal (?)",
     'v': "hishthaphel (?)",
     'z': "hitpoel (?)",
+}
+
+def parse_oshm_verb(subMorph):
+    return "verbe ({})".format(tences.get(subMorph[1], '?'))
+
+noun_type = {
+    'c': "nom commun",
+    'g': "gentilé"
+}
+
+noun_genre = {
+    'm': "masc.",
+    'f': "fém.", 
+    'b': "masc. et fém."
+}
+
+noun_form = {
+    's': "sing.",
+    'p': "pl.",
+    'd': "duel"
+}
+
+noun_state = {
+    'c': "const.",
+    'a': "abs."
+}
+
+def parse_oshm_noun(subMorph):
+    pattern_noun = re.compile(r'N(p)|N((?P<type>g|c)(?P<genre>m|f|b)(?P<form>s|p|d)(?P<state>c|a))')
+    match_noun = pattern_noun.match(subMorph)
+
+    if match_noun is not None:
+        if match_noun.group(2) is None:
+            return "nom propre"
+        else:
+            return "{} {} {} (état {})".format(
+                noun_type[match_noun.group("type")],
+                noun_genre[match_noun.group("genre")],
+                noun_form[match_noun.group("form")],
+                noun_state[match_noun.group("state")])
+
+    return "nom"
+
+wordClasses = {
+    'A': lambda x: "adjectif",
+    'C': lambda x: "conjonction",
+    'D': lambda x: "adverbe",
+    'N': parse_oshm_noun,
+    'P': lambda x: "pronom démonstratif",
+    'R': lambda x: "préposition",
+    'S': lambda x: "suffixe",
+    'T': lambda x: "particule",
+    'V': parse_oshm_verb,
 }
