91ba2b000c53a4c0081c95e80dc7cf7f518c2dee
Antoine <antoine@2ohm.fr>
2021-06-30 16:49:02 +0200

    Use the index to generate table of content

--- theke/tableofcontent.py
+++ theke/tableofcontent.py
@@ -4,8 +4,26 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 
+import theke.uri
+import theke.index
+
 # from collections import namedtuple
 # tocFields = namedtuple('tocFields',['name','rawUri'])
+
+def get_toc_from_ref(ref):
+    #TODO:  /!\ Suppose que la référence est une référence biblique.
+    #       Cette fonction devra être adaptée aux livres non bibliques.
+
+    thekeIndex = theke.index.ThekeIndex()
+    nbOfChapters = thekeIndex.get_document_nbOfSections(ref.documentName)
+
+    toc = theke.tableofcontent.ThekeTOC()
+
+    for i in range(nbOfChapters):
+        uri = theke.uri.build('sword', ['', theke.uri.SWORD_BIBLE, '{} {}'.format(ref.documentName, i+1)])
+        toc.append(str(i+1), uri.get_encoded_URI())
+
+    return toc
 
 class ThekeTOC():
     def __init__(self):
