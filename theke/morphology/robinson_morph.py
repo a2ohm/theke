470a6442de0a44382a95e4be05fac144bd7c7f86
cibvia <viacia@hotmail.fr>
2021-11-19 00:24:55 +0100

    parse declination and conjugation

--- theke/morphology/robinson_morph.py
+++ theke/morphology/robinson_morph.py
@@ -28,31 +28,94 @@ voice = {
     'P': "passif",
 }
 
+person = {
+    '1': "1e personne",
+    '2': "2e personne",
+    '3': "3e personne",
+}
+
+number = {
+    'S': "singulier",
+    'P': "pluriel",
+}
+
+case = {
+    'N': "nominatif",
+    'V': "vocatif",
+    'A': "accusatif",
+    'G': "génitif",
+    'D': "datif",
+}
+
+gender = {
+    'M': "masculin",
+    'F': "féminin",
+    'N': "neutre",
+}
+
 def parse_robinson_verb(subMorph):
     # submorph = [tence / voice / mood][person / number]
     # submorph = [tence / voice / mood][case / number / gender]
 
     tvm = subMorph[0]
+    ssm = subMorph[1]
+    ssmAnalysed = '?'
+    if len(ssm) == 2:
+        ssmAnalysed = parse_robinson_conjugation(ssm)
+    elif len(ssm) == 3:
+        ssmAnalysed = parse_robinson_declination([ssm], "verbe")
+
     tvmAnalysed = "{} {} {}".format(
         mood.get(tvm[-1], '?'),
         tence.get(tvm[:-2], '?'),
-        voice.get(tvm[-2], '?'))
+        voice.get(tvm[-2], '?')
+        )
 
-    return "verbe ({})".format(tvmAnalysed)
+
+
+    return "verbe ({}, {})".format(tvmAnalysed, ssmAnalysed)
+
+def parse_robinson_conjugation(pn):
+    return "{} du {}".format(
+            person.get(pn[0], '?'),
+            number.get(pn[1], '?')
+            ) 
+
+def parse_robinson_declination(subMorph, wordType):
+    
+    cng = subMorph[0]
+    cngAnalysed = "{} {} {}".format(
+        case.get(cng[0], '?'),
+        gender.get(cng[2], '?'),
+        number.get(cng[1], '?') 
+        )
+    if wordType == "verbe":
+        return cngAnalysed
+    else:
+        return "{}, {}".format(
+            wordType,
+            cngAnalysed
+        )
+
+
 
 wordClasses = {
-    'A': lambda x: "adjectif",
+    'A': lambda x: parse_robinson_declination(x, "adjectif"),
+    'ADV': lambda x: "adverbe",
     'C': lambda x: "pronom réciproque",
+    'CONJ': lambda x: "conjonction",
     'D': lambda x: "pronom démonstratif",
     'F': lambda x: "pronom réfléchit",
     'I': lambda x: "pronom interrogatif",
     'K': lambda x: "pronom corrélatif",
-    'N': lambda x: "nom",
+    'N': lambda x: parse_robinson_declination(x, "nom"),
     'P': lambda x: "pronom personnel",
+    'PREP': lambda x: "préposition",
+    #'PRT': lambda x: "inconnu", ??? cf. ποτε He 1, 5
     'Q': lambda x: "pronom corrélatif ou interrogatif",
-    'R': lambda x: "pronom relatif",
+    'R': lambda x: parse_robinson_declination(x, "pronom relatif"),
     'S': lambda x: "pronom possessif",
-    'T': lambda x: "article défini",
+    'T': lambda x: parse_robinson_declination(x, "article défini"),
     'V': parse_robinson_verb,
     'X': lambda x: "pronom indéfini",
 }
